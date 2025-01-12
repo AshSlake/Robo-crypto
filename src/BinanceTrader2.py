2d7c82b6e3c6d1ea607c1af41c9edfcabff16222
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-01-12 20:37:49 -0300

    feat: adicionado getActualTradePositionForBinance para mais precisao em obter as ultimas transações

--- src/BinanceTrader2.py
+++ src/BinanceTrader2.py
@@ -88,6 +88,23 @@ class BinanceTraderBot:
         else:
             return False # Está vendido
         
+
+    def getActualTradePositionForBinance(self):
+        try:
+            trades = self.client_binance.get_my_trades(symbol=self.operation_code, limit=1)
+            if trades:
+                last_trade = trades[0]
+                return last_trade['isBuyer']  # True se a última ordem foi compra, False se foi venda
+            else:
+                # Se não houver negociações, assume que a posição é vendida (ou neutra)
+                return False 
+        except BinanceAPIException as e:
+            erro_logger.exception(f"Erro na Binance API ao obter a posição de negociação: {e}")
+            return False  # Retorna False em caso de erro para evitar compras acidentais
+        except Exception as e:
+            erro_logger.exception(f"Erro ao obter a posição de negociação: {e}")
+            return False   
+         
     # Prints
 
     def printAllWallet(self):
