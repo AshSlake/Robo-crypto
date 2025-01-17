a06ad50f362eed077e777fb14f94699ab8e4d5d2
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-01-17 16:47:13 -0300

    feat: adicionado documentação na classe RsiCalculation

--- src/functions/RsiCalculationClass.py
+++ src/functions/RsiCalculationClass.py
@@ -1,39 +1,78 @@
 import pandas as pd
+from functions.logger import erro_logger
 
 
 class TechnicalIndicators:
+    """
+    Classe para calcular indicadores técnicos.
+
+    Attributes:
+        stock_data (pd.DataFrame): DataFrame contendo os dados do ativo, com uma coluna 'close_price'.
+        rsi_period (int): Período para o cálculo do RSI (padrão: 14).
+
+    Methods:
+        calculate_rsi(): Calcula o Índice de Força Relativa (RSI) e adiciona ao DataFrame.
+    """
+
     def __init__(self, stock_data, rsi_period=14):
+        """
+        Inicializa a classe TechnicalIndicators.
+
+        Args:
+            stock_data (pd.DataFrame): DataFrame com os dados do ativo. Deve conter uma coluna 'close_price'.
+            rsi_period (int): Período para cálculo do RSI. Padrão: 14.
+        """
         self.stock_data = stock_data
         self.rsi_period = rsi_period
 
     def calculate_rsi(self):
-        # Converte a coluna 'close_price' para numérico, substituindo erros por NaN
-        self.stock_data["close_price"] = pd.to_numeric(
-            self.stock_data["close_price"], errors="coerce"
-        )
+        """
+        Calcula o Índice de Força Relativa (RSI).
 
-        # Calcula a diferença entre os preços de fechamento
-        delta = self.stock_data["close_price"].diff()
+        Calcula o RSI usando a média móvel exponencial suavizada (SMMA) para ganhos e perdas.
+        Adiciona uma nova coluna 'rsi' ao DataFrame stock_data.
 
-        # Calcula os ganhos e perdas
-        gain = delta.where(delta > 0, 0)
-        loss = -delta.where(delta < 0, 0)
+        Returns:
+            pd.DataFrame: O DataFrame stock_data com a coluna 'rsi' adicionada.
+            None: se ocorrer algum erro.
 
-        # Calcula a SMMA dos ganhos e perdas
-        avg_gain = gain.ewm(span=self.rsi_period, adjust=False).mean()
-        avg_loss = loss.ewm(span=self.rsi_period, adjust=False).mean()
+        Raises:
+            TypeError: Se stock_data não for um pandas DataFrame ou não contiver uma coluna 'close_price'.
+            ValueError: se dados inválidos forem encontrados em 'close_price'
+        """
+        if not isinstance(self.stock_data, pd.DataFrame):
+            raise TypeError("stock_data deve ser um pandas DataFrame.")
 
-        # Calcula o RS e o RSI
-        rs = avg_gain / avg_loss
-        rsi = 100 - (100 / (1 + rs))
+        if "close_price" not in self.stock_data.columns:
+            raise TypeError("stock_data deve conter a coluna 'close_price'.")
 
-        # Adiciona o RSI ao DataFrame
-        self.stock_data["rsi"] = rsi
-        return self.stock_data
+        try:
 
+            # Converte a coluna 'close_price' para numérico, substituindo erros por NaN
+            self.stock_data["close_price"] = pd.to_numeric(
+                self.stock_data["close_price"], errors="coerce"
+            )
 
-# Exemplo de uso
-# stock_data = pd.DataFrame({'close_price': [dados_aqui]})
-# ti = TechnicalIndicators(stock_data, rsi_period=14)
-# rsi_values = ti.calculate_rsi()
-# print(stock_data)
+            # Calcula a diferença entre os preços de fechamento
+            delta = self.stock_data["close_price"].diff()
+
+            # Calcula os ganhos e perdas
+            gain = delta.where(delta > 0, 0)
+            loss = -delta.where(delta < 0, 0)
+
+            # Calcula a SMMA dos ganhos e perdas
+            avg_gain = gain.ewm(span=self.rsi_period, adjust=False).mean()
+            avg_loss = loss.ewm(span=self.rsi_period, adjust=False).mean()
+
+            # Calcula o RS e o RSI
+            rs = avg_gain / avg_loss
+            rsi = 100 - (100 / (1 + rs))
+
+            # Adiciona o RSI ao DataFrame
+            self.stock_data["rsi"] = rsi
+            return self.stock_data
+
+        except (ValueError, TypeError) as e:
+            erro = f"Erro ao tentar executar calculate_rsi em Technical Indicator: {e}"
+            erro_logger.error(erro)  # Registra a mensagem de erro no bot_logger.
+            return None  # Retorna None em caso de erro.
